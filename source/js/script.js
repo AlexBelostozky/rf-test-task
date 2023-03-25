6585908bc13cf6dcf3c3cdbabfff2a485cfbf8fe
Александр Белостоцкий <belostozky1994@mail.ru>
2023-03-25 16:32:02 +0600

    Add masonry grid setup

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,15 @@
 import {getData} from './api.js';
 
-getData(console.log);
+// getData(console.log);
+
+const masonryGridContainer = document.querySelector('.gallery__list');
+const masonryGridOptions = {
+  itemSelector: '.gallery__item-container',
+  // columnWidth: 476,
+  gutter: 27,
+  // fitWidth: true,
+  // columnWidth: 330,
+  // resize: true
+}
+
+new Masonry(masonryGridContainer, masonryGridOptions);
